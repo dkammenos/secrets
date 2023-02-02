20920d434eaa24779c463ed1707b64ffbfdb0cc0
ΔΙΟΝΥΣΙΟΣ ΚΑΜΜΕΝΟΣ <std080228@ac.eap.gr>
2023-02-02 11:59:06 +0200

    Level 5 cookies & session

--- app.js
+++ app.js
@@ -6,32 +6,50 @@ const ejs = require("ejs");
 const mongoose = require('mongoose');
 //const encrypt = require("mongoose-encryption" Level)
 //const md5 = require("md5" Lever 3);
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+//Level 4 bcrypt
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+//Level 5 - Session & Cookies
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose")
 
 const app = express();
 
-console.log(process.env.API_KEY);
+app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-app.use(express.static("public"));
 
+app.use(session({ 	
+    secret: "Our little secret",
+    resave: false, 
+    saveUninitialized: false }
+  ));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 //TODO
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/userDB',{useNewUrlParser:true});
+
 
 const userSchema = new mongoose.Schema( {
     email:String,
     password:String
 });
+userSchema.plugin(passportLocalMongoose);
+
 
 secret=process.env.SECRET;
 
 //userSchema.plugin(encrypt,{secret:secret, encryptedFields:["password"]});
 
 const User = mongoose.model("User",userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser());
 
 
 app.get("/",(req, res)=> {
@@ -47,60 +65,51 @@ app.get("/",(req, res)=> {
     res.render("login");
   
   });
+
+
+
 app.get("/register",(req, res)=> {
-  res.render("register");
   
   });
-  app.get("/logout",(req, res)=> {
-    res.render("home");
-    
+  app.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/');
     });
+  });
+
+    app.get("/secrets", function(req, res){
+      if (req.isAuthenticated()){ 
+       res.render("secrets");
+       } else {
+       res.redirect("/login") 
+       }
+       })
 
     //password: md5(req.body.password)
   app.post("/register",(req, res)=> {
-    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-      const newUser = new User({
-        email:req.body.username,
-        password: hash
-        });
-          newUser.save((err)=> {
-            if (err) {
-              console.log(err)
-            } else {
-              res.render("secrets");
-            }
-            });    
-      // Store hash in your password DB.
-  });
+    User.register({username: req.body.username}, req.body.password, function(err, user){ if (err) {
+      console.log(err);
+      res.redirect("/register");
+      } else {
+      passport.authenticate("local") (req, res, function(){
+      res.redirect("/secrets");
+      });
+      }
+});
 
    
     });
 
-    app.post("/login",(req, res)=> {
-      username = req.body.username;
-      //password = md5(req.body.password);
-      password = req.body.password;
-      User.findOne({email:username}, function (err, foundUser){
-        if (err) {
-          console.log("lathos");
-          res.render("home")
-        }else {
-          if(foundUser) {
-            bcrypt.compare(password,foundUser.password, function(err, result) {
-              if (result == true) {
-                console.log(password);
-                res.render("secrets")
-              }
-            
-              
-              // result == false
-          });
-           
-          }
-        }
+    app.post("/login", function(req, res){
+      const user = new User({ username: req.body.username, password: req.body.password });
+      req.login(user, function(err){ if (err) {
+      console.log(err);
+      } else { passport.authenticate("local")(req, res, function(){ 
+        res. redirect("/secrets");
+      }); }
       });
     });
-
      
 
 app.listen(process.env.PORT || 3000, () => {
